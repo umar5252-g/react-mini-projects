a58e171de1b216ac37c3aa69fc2364c2a94802e2
umar5252-g <mromertariq@gmail.com>
2025-12-24 10:20:15 +0500

    create delete , save and display features

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,49 +1,84 @@
 // import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
+function TodoInput({ todos, setTodos }) {
+  const [inputTodo, setInputTodo] = useState("hello guys how can i help you ");
+
+  return (
+    <>
+      {" "}
+      <input
+        className="todo-input"
+        type="text"
+        placeholder="Enter your todo here"
+        value={inputTodo}
+        onChange={(event) => {
+          setInputTodo(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const newTodo = inputTodo;
+          setTodos([...todos, newTodo]);
+          setInputTodo("");
+          // console.log(todos);
+        }}
+        className="save-btn"
+      >
+        Save
+      </button>
+    </>
+  );
+}
+
+function DisplayTodo({ todos, setTodos }) {
+  return (
+    <div>
+      <div className="todos-container">
+        {todos.map((item, index) => {
+          return (
+            <div key={index} className="todo">
+              {item}
+              <div className="delete-btn">
+                <button
+                  onClick={() => {
+                    const idxToDelete = index;
+                    const newTodos = todos.filter(
+                      (todo, i) => i !== idxToDelete
+                    );
+                    setTodos(newTodos);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 function App() {
-  // useEffect(() => {}, []);
-  function TodoInput() {
-    return (
-      <>
-        {" "}
-        <input
-          className="todo-input"
-          type="text"
-          placeholder="Enter your todo here"
-          onChange={() => {}}
-        />
-        <button onClick={() => {}} className="save-btn">
-          Save
-        </button>
-      </>
-    );
-  }
-  function DisplayTodo() {
-    return (
-      <>
-        <div className="todos-container">
-          <div className="todo">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic
-            obcaecati quisquam atque eos voluptatum! Porro consequuntur at illum
-            animi. Doloremque impedit saepe nisi et! Voluptates autem nulla
-            mollitia laboriosam culpa.
-            <div className="delete-btn">
-              <button>Delete</button>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
+  const [todos, setTodos] = useState([]);
+
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const OddNum = nums.filter((num) => {
+    return num % 2 !== 0;
+  });
+  console.log(OddNum);
+  const numbers = [1, 2, 3, 4, 5, 6];
+  const oddNumbers = numbers.filter((num) => num % 2 !== 0);
+  console.log(oddNumbers); // [1, 3, 5]
 
   return (
     <>
       <div className="app-container">
         <h1>TODO APP</h1>
         <br />
-        <TodoInput />
-        <DisplayTodo />
+        <TodoInput todos={todos} setTodos={setTodos} />
+        <DisplayTodo todos={todos} setTodos={setTodos} />
       </div>
     </>
   );
