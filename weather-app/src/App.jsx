797915f82265f4e8128ff32073d640346fa2e1bd
umar5252-g <mromertariq@gmail.com>
2025-12-27 08:50:05 +0500

    add logic to weather app

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  let display;
+  const [city, setCity] = useState("peshawar");
+  const [data, setData] = useState(null);
+  // let url = `https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=${apiKey}&units=metric`;
+  useEffect(() => {
+    if (!city) return;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=016de935b9164dc30cf79bc99d3cff55&units=metric`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log("Error while fetching the data", error);
+      }
+    };
+    fetchData();
+  }, [city]);
 
   return (
     <div className="app-containers">
@@ -19,14 +36,28 @@ function App() {
         />
         <button
           onClick={() => {
-            display = input;
+            setCity(input);
             setInput("");
           }}
         >
           Search
         </button>
       </div>
-      <div className="weather-infor">{display}</div>
+      <div className="weather-infor">
+        <div className="temperature">
+          {" "}
+          Temperature : {data && data.main.temp}
+        </div>
+        <div className="icon">
+          <img
+            src={`https://openweathermap.org/img/wn/${
+              data && data.weather[0].icon
+            }@2x.png`}
+            alt="weather icon"
+          />
+        </div>
+        <div className="humidity">Humidity : {data && data.main.humidity}</div>
+      </div>
     </div>
   );
 }
