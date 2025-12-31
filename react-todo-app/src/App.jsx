443d0e9a396a83669f019760e3e2b1ffb2dbb70b
umar5252-g <mromertariq@gmail.com>
2025-12-31 17:07:22 +0500

    add local storage feature

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,22 +1,31 @@
 // import { useEffect, useState } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoInput } from "./components/TodoInput.jsx";
 import { DisplayTodo } from "./components/DisplayTodo.jsx";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "Complete homeWork",
-      completed: false,
-    },
-    {
-      id: 2,
-      text: "Buy milk",
-      completed: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos");
+    return saved
+      ? JSON.parse(saved)
+      : [
+          {
+            id: 1,
+            text: "Complete homeWork",
+            completed: false,
+          },
+          {
+            id: 2,
+            text: "Buy milk",
+            completed: false,
+          },
+        ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const addTodo = (text) => {
     const newTodo = {
       id: crypto.randomUUID(),
